ad24154b63385b631ab398b7fbf184d19e3225b3
Leonardo Fadul <lsffadul@gmail.com>
2024-08-02 15:14:28 -0300

    Using JInput in main

--- app/main.c
+++ app/main.c
@@ -2,8 +2,8 @@
 //
 
 #include <stdio.h>
-#include "joj/error_code.h"
 #include "joj/platform/platform.h"
+#include "joj/platform/input.h"
 
 int main()
 {
@@ -13,11 +13,28 @@ int main()
         return FAIL;
     }
 
+    input_init();
+
     b8 running = true;
     while (running) {
         if (!platform_process_events(&plat))
             running = false;
+
+        if (input_is_key_pressed(KEY_ESCAPE)) {
+            running = false;
+        }
+
+        if (input_is_key_pressed('A')) {
+            printf("A pressed.\n");
+        }
+
+        if (input_is_key_down(KEY_SPACE)) {
+            printf("Space down.\n");
+        }
     }
+
+    input_shutdown();
+    platform_shutdown(&plat);
 
     print_from_joj();
 	return 0;
