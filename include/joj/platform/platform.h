da673e6d3bfae72c7d656a9bfc3492c1502275cf
Leonardo Fadul <lsffadul@gmail.com>
2024-08-02 14:30:18 -0300

    Implemented platform for win32

--- include/joj/platform/platform.h
+++ include/joj/platform/platform.h
@@ -4,6 +4,21 @@
 #define JOJ_ENGINE_IMPLEMENTATION
 #include "defines.h"
 
+#include "error_code.h"
+
+typedef enum WindowMode_enum { BORDERLESS, FULLSCREEN, WINDOWED } WindowMode;
+
+struct JWindow;
+
+typedef struct JPlatformManager_st
+{
+    struct JWindow* window;
+} JPlatformManager;
+
+JAPI ErrorCode platform_init(JPlatformManager* platform_manager, u16 width, u16 height, const char* title, WindowMode mode);
+JAPI void platform_shutdown(JPlatformManager* platform_manager);
+JAPI b8 platform_process_events(JPlatformManager* platform_manager);
+
 JAPI void print_from_joj();
 
 #endif // JOJ_PLATFORM_H
