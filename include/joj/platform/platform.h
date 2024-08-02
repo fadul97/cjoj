e8af3544e4f693f139808a088772658876b0ac15
Leonardo Fadul <lsffadul@gmail.com>
2024-08-02 12:31:34 -0300

    Setting up platform header library

---
+++ include/joj/platform/platform.h
@@ -0,0 +1,11 @@
+#ifndef JOJ_PLATFORM_H
+#define JOJ_PLATFORM_H
+
+#include <stdio.h>
+void print_from_joj()
+{
+    const i32 n = 100;
+    printf("Print from joj. n = %d\n", n);
+}
+
+#endif // JOJ_PLATFORM_H
