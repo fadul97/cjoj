9bf8c81bf0edc8a28ac85b8d37475e510997a5a2
Leonardo Fadul <lsffadul@gmail.com>
2024-08-02 12:36:19 -0300

    Forgot to add changes in platform.h

--- include/joj/platform/platform.h
+++ include/joj/platform/platform.h
@@ -1,11 +1,6 @@
 #ifndef JOJ_PLATFORM_H
 #define JOJ_PLATFORM_H
 
-#include <stdio.h>
-void print_from_joj()
-{
-    const i32 n = 100;
-    printf("Print from joj. n = %d\n", n);
-}
+void print_from_joj();
 
 #endif // JOJ_PLATFORM_H
